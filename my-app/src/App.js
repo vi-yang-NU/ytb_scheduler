49b5ec7c4ca852b8596ea67391f86e24bd7e184d
Vincent-Yang0134 <vincentyang2026@u.northwestern.edu>
2024-12-14 09:21:10 +0800

    Working code for navigation bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import DashboardSection from './components/DashboardSection';
 import QueueSection from './components/QueueSection';
+import Navigation from './components/navigation';
 
 const App = () => {
   const [viewMode, setViewMode] = useState('grid');
@@ -30,19 +31,26 @@ const App = () => {
 
   return (
     <div className="min-h-screen bg-blue-900 p-6">
-      <div className="max-w-[80%] mx-auto">
-        <DashboardSection />
-        <QueueSection
-          viewMode={viewMode}
-          setViewMode={setViewMode}
-          isDraft={isDraft}
-          handleDrop={handleDrop}
-          handleDragOver={handleDragOver}
-          handleFileSelect={handleFileSelect}
-          currentStep={currentStep}
-          setCurrentStep={setCurrentStep} // Pass setCurrent
-          nextStep={nextStep}
-        />
+      <div className="grid grid-cols-10 gap-4 max-w-full mx-auto h-full">
+        {/* Left 10% Navigation */}
+        <div className="col-span-1 h-full">
+          <Navigation />
+        </div>
+        {/* Right 80% Main Content */}
+        <div className="col-span-9">
+          <DashboardSection />
+          <QueueSection
+            viewMode={viewMode}
+            setViewMode={setViewMode}
+            isDraft={isDraft}
+            handleDrop={handleDrop}
+            handleDragOver={handleDragOver}
+            handleFileSelect={handleFileSelect}
+            currentStep={currentStep}
+            setCurrentStep={setCurrentStep}
+            nextStep={nextStep}
+          />
+        </div>
       </div>
     </div>
   );
